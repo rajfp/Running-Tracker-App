27bf3bca3cfa17f9147a512a8668edba9334cdac
Rajat Chauhan <rajat1.chauhan@airtelbank.com>
2021-01-27 23:47:57 +0530

    added navigation componeny

--- RunningTrackerApp/app/src/main/java/com/example/runningtrackerapp/ui/MainActivity.kt
+++ RunningTrackerApp/app/src/main/java/com/example/runningtrackerapp/ui/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.runningtrackerapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.runningtrackerapp.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(nav_host_fragment.findNavController())
+        nav_host_fragment.findNavController().addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+
+                R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+                else -> bottomNavigationView.visibility = View.GONE
+            }
+
+        }
     }
 }
