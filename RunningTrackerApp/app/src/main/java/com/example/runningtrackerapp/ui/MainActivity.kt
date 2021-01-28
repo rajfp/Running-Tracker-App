2598c196b7f2c8a5574cc016f9d9636a26353d89
Rajat Chauhan <rajat1.chauhan@airtelbank.com>
2021-01-29 00:38:53 +0530

    added foreground service and map

--- RunningTrackerApp/app/src/main/java/com/example/runningtrackerapp/ui/MainActivity.kt
+++ RunningTrackerApp/app/src/main/java/com/example/runningtrackerapp/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.runningtrackerapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.runningtrackerapp.R
+import com.example.runningtrackerapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigateToTrackingFragmentIfNeeded(intent)
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(nav_host_fragment.findNavController())
         nav_host_fragment.findNavController().addOnDestinationChangedListener { _, destination, _ ->
@@ -27,5 +30,15 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+    private fun navigateToTrackingFragmentIfNeeded(intent:Intent?){
+        if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT ){
+            nav_host_fragment.findNavController().navigate(R.id.action_global_tracking_fragment)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
     }
 }
